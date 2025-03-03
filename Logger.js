14869be3c38e23e1853c78bdabea08869e521139
xrousset78800 <xrousset@alchemy.fr>
2025-03-03 15:06:03 +0100

    Modification du fichier - Logger.js -

--- Logger.js
+++ Logger.js
@@ -1,18 +1,45 @@
-// Logger.js
-
 class Logger {
-  constructor() {}
+  constructor() {
+    this.logLevel = 'DEBUG'; // default log level
+    this.consoleLogEnabled = true;
+    this.fileLogEnabled = false;
 
-  log(message) {
-    console.log(`[${new Date().toLocaleString()}] ${message}`);
+    if (process.env.NODE_ENV === 'production') {
+      this.logLevel = 'ERROR';
+      this.consoleLogEnabled = false;
+      this.fileLogEnabled = true;
+    }
   }
 
-  error(errorMessage) {
-    this.log('Error: ' + errorMessage);
+  log(message, ...args) {
+    if (this consoleLogEnabled && this.getLogLevel() >= 'DEBUG') {
+      console.log(`[${new Date().toISOString()}] ${message}`, ...args);
+    }
   }
 
-  warn(warningMessage) {
-    this.log('Warning: ' + warningMessage);
+  error(message, ...args) {
+    if (this.consoleLogEnabled && this.getLogLevel() >= 'ERROR') {
+      console.error(`[${new Date().toISOString()}] ${message}`, ...args);
+    }
+  }
+
+  getLogLevel() {
+    switch (this.logLevel.toUpperCase()) {
+      case 'DEBUG':
+        return 3;
+      case 'INFO':
+        return 2;
+      case 'WARNING':
+        return 1;
+      case 'ERROR':
+        return 0;
+      default:
+        throw new Error('Invalid log level');
+    }
+  }
+
+  setLogLevel(level) {
+    this.logLevel = level.toUpperCase();
   }
 }
 
