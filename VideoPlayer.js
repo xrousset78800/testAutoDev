48a6b25dddadb18c4b180f5ce5e4a7c327805651
xrousset78800 <xrousset@alchemy.fr>
2025-03-03 14:45:25 +0100

    Modification du fichier - VideoPlayer.js -

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1,62 +1,53 @@
 // VideoPlayer.js
-import { HLSService } from './HLSService';
-import { ProxyService } from './ProxyService';
-import { MediaLoader } from './MediaLoader';
-import { StatusDisplay } from './StatusDisplay';
-import { QualitySelector } from './QualitySelector';
-import { DebugPanel } from './DebugPanel';
 
 class VideoPlayer {
-  constructor() {
-    this.statusDisplay = new StatusDisplay();
-    this.qualitySelector = new QualitySelector();
-    this.debugPanel = new DebugPanel();
-    this.mediaLoader = new MediaLoader();
-    this.hlsservice = new HLSService();
-    this.proxyService = new ProxyService();
+  constructor(mediaLoader, statusDisplay, qualitySelector, debugPanel) {
+    this.mediaLoader = mediaLoader;
+    this.statusDisplay = statusDisplay;
+    this.qualitySelector = qualitySelector;
+    this.debugPanel = debugPanel;
 
-    // Initialize singleton pattern for services and components
-    VideoPlayer.instance = this;
+    // Initialize video player state
+    this.currentMediaId = null;
+    this.currentQualityLevel = 0;
+    this.isPlaying = false;
   }
 
-  loadVideo(url) {
-    const mediaType = detectMediaType(url);
-    if (mediaType === 'hls') {
-      return this.hlsservice.loadHLS(url);
-    } else if (mediaType === 'mpeg-ts') {
-      return this.mediaLoader.loadMPEGTS(url);
+  play(mediaId) {
+    if (this.currentMediaId !== mediaId) {
+      this.stop();
+      this.currentMediaId = mediaId;
+      this.mediaLoader.loadmedia(this.currentMediaId);
     } else {
-      // Assume MP4 format
-      return this.mediaLoader.loadMP4(url);
+      // Resume playback
+      this.isPlaying = true;
     }
   }
 
-  playVideo(videoUrl) {
-    const video = this.loadVideo(videoUrl);
-    if (video) {
-      // Play the video using HTML5 Video API or other playback libraries
-      console.log('Playing video:', videoUrl);
-    } else {
-      this.statusDisplay.displayError('Failed to load video');
+  stop() {
+    this.isPlaying = false;
+  }
+
+  updateStatus(statusMessage) {
+    this.statusDisplay.updateStatus(statusMessage);
+  }
+
+  selectQuality(qualityLevel) {
+    if (this.currentQualityLevel !== qualityLevel) {
+      // Update video player state
+      this.currentQualityLevel = qualityLevel;
+
+      // Notify media loader to adjust playback based on new quality level
+      this.mediaLoader.setQuality(this.currentMediaId, this.currentQualityLevel);
     }
   }
 
-  getQualityOptions() {
-    return this.qualitySelector.getQualityOptions();
-  }
-
-  setQualityOption(quality) {
-    // Update the quality option and re-render the video
-    console.log(`Setting quality to: ${quality}`);
-  }
-
-  debugInfo() {
-    const info = {
-      resolution: '1920x1080',
-      bitrate: '5000 kbps',
-      latency: '100 ms'
+  getDebugInfo() {
+    return {
+      currentMediaId: this.currentMediaId,
+      currentQualityLevel: this.currentQualityLevel,
+      isPlaying: this.isPlaying,
     };
-    this.debugPanel.displayDebugInfo(info);
   }
 }
 
