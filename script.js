c42b47254667b573b3f73d393690fd49a2edb851
xrousset78800 <xrousset@alchemy.fr>
2025-03-04 17:37:57 +0100

    Modification du fichier -

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 file: {
 action: "update",
-name: index.html,
-actionDescription: "Add an h1 tag with 'Hello World' text and update the script to animate it"
-},
-
-file: {
-action: "create",
-name: style.css,
-actionDescription: "Create a new CSS file for styling the animation"
+name: script.js,
+actionDescription: "Add a bounce effect to the 'Hello World' text when it appears"
 }
