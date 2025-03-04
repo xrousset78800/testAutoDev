e11e91ff0fdbb51c76e72225d3af1d5853571bc0
xrousset78800 <xrousset@alchemy.fr>
2025-03-04 22:22:28 +0100

    Modification du fichier - VideoPlayer.js

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -1,55 +1,7 @@
 class VideoPlayer {
-    constructor() {
-        this.container = null;
-        this.source = null;
-        this.isPlaying = false;
-        this.quality = 'auto';
-        this.mediaLoader = new MediaLoader();
-    }
-
-    setContainer(container) {
-        this.container = container;
-    }
-
-    setSource(source) {
-        this.source = source;
-    }
-
-    play() {
-        if (!this.isPlaying) {
-            // Play the video
-            this.container.play();
-
-            this.isPlaying = true;
-
-            Logger.log('Playing');
-        }
-    }
-
-    pause() {
-        if (this.isPlaying) {
-            // Pause the video
-            this.container.pause();
-
-            this.isPlaying = false;
-
-            Logger.log('Pausing');
-        }
-    }
-
-    setQuality(quality) {
-        this.quality = quality;
-        // Update the video player with the new quality
-        this.mediaLoader.setQuality(quality);
-    }
-
-    isPlaying() {
-        return this.isPlaying;
-    }
-
-    getContainer() {
-        return this.container;
-    }
+  constructor() {
+    this.style = require('./style.css');
+  }
+  
+  // Your video player logic here...
 }
-
-export default VideoPlayer;
