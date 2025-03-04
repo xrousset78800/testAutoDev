0d91cb40130799dc3946b73e2f8edcdb3f9999fd
xrousset78800 <xrousset@alchemy.fr>
2025-03-04 17:37:39 +0100

    Création du fichier - script.js -

---
+++ script.js
@@ -0,0 +1,11 @@
+file: {
+action: "update",
+name: index.html,
+actionDescription: "Add an h1 tag with 'Hello World' text and update the script to animate it"
+},
+
+file: {
+action: "create",
+name: style.css,
+actionDescription: "Create a new CSS file for styling the animation"
+}
