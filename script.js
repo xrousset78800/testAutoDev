385e4f05ed3906600af4fa951a1972352ef89611
xrousset78800 <xrousset@alchemy.fr>
2025-03-04 17:31:08 +0100

    Création du fichier - script.js -

---
+++ script.js
@@ -0,0 +1,14 @@
+file : {
+  action: "update",
+  name: style.css,
+  actionDescription: "Add styles for the 'hello' element and make it blink"
+}
+
+Code:
+```
+#hello {
+  font-size: 48px;
+  color: #00698f;
+  text-decoration: blink;
+}
+```
