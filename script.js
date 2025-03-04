e735e51c76ef7f6f7e89920306c57a94e3155b53
xrousset78800 <xrousset@alchemy.fr>
2025-03-04 17:28:59 +0100

    hard reset

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    console.log('Hello World!');
-    
-    let i = 0;
-    setInterval(() => {
-        document.body.innerHTML += '<span style="font-size: ' + (i++ * 10) + 'px;">' + 'Hello World!' + '</span>';
-    }, 100);
-</script>
