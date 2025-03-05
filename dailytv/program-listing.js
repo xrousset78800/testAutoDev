b4c711bde4cd143f0bd3f42780a332b43405c1f8
xrousset78800 <xrousset@alchemy.fr>
2025-03-05 22:27:51 +0100

    Modification du fichier - dailytv/program-listing.js

--- dailytv/program-listing.js
+++ dailytv/program-listing.js
@@ -1,17 +1,6 @@
-// Load data from JSON file
-fetch('.data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Get the program listing element
-    const programListing = document.getElementById('program-listing');
-
-    // Create a table to display programs
-    let html = '<table>';
-    for (const program of data) {
-      html += `<tr><td>${program.name}</td><td>${program.time}</td></tr>`;
-    }
-    html += '</table>';
-
-    // Add the HTML content to the page
-    programListing.innerHTML = html;
-  });
+{
+  action: "modify", 
+  name: "program-listing.js", 
+  path: "dailytv/program-listing.js", 
+  actionDescription: "Modify the JavaScript file to parse and display JSON data"
+},
