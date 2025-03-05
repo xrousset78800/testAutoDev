a26d65e25397c5e7eb9328be50d37ea849bf853d
xrousset78800 <xrousset@alchemy.fr>
2025-03-05 01:43:08 +0100

    Modification du fichier - url-helper.js

--- url-helper.js
+++ url-helper.js
@@ -1,14 +1,15 @@
-
 class UrlHelper {
   constructor() {}
 
   getAbsoluteUrl(url) {
+    if (url.startsWith('./')) {
+      url = window.location.href.split('/').slice(0, -1).join('/') + '/' + url.substring(2);
+    }
     return new URL(url, window.location.href).href;
   }
 
   getUrlParameter(name) {
-    const url = this.getAbsoluteUrl(window.location.href);
-    const params = new URLSearchParams(new URI.parse(url).query);
+    const params = new URLSearchParams(window.location.search);
 
     if (!params.has(name)) {
       return null;
