76dd513cf95e9f5eb563a0abc27cb43575708dcc
xrousset78800 <xrousset@alchemy.fr>
2025-03-06 00:29:07 +0100

    hard reset

--- dailytv/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Program(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    time = models.DateTimeField()
-
-    def __str__(self):
-        return self.name
