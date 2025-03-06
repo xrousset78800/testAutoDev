ec060090c40dfac9540c756a045ee2d1f7bd228a
xrousset78800 <xrousset@alchemy.fr>
2025-03-06 05:34:12 +0100

    Modification du fichier - dailytv/api.js

--- dailytv/api.js
+++ dailytv/api.js
@@ -1,15 +1,19 @@
-// Import the program.json file as a JSON object
-const programData = require('./program.json');
+// api.js
+const axios = require('axios');
 
-// Define an API endpoint to retrieve daily TV program data
-function getDailyProgram() {
-  return new Promise((resolve, reject) => {
-    // Simulate API request (replace with your actual API call)
-    setTimeout(() => {
-      resolve(programData);
-    }, 1000); // Replace with the actual time it takes for the API to respond
-  });
+function getProgrammes() {
+    return axios.get('./programme.json')
+        .then(response => response.data);
 }
 
-// Export the getDailyProgram function
-module.exports = {getDailyProgram};
+function addComments(programmes) {
+    const comments = [];
+    
+    programmes.forEach(programme => {
+        comments.push(`**${programme.name}**: ${programme.description}`);
+    });
+    
+    return comments;
+}
+
+module.exports = { getProgrammes, addComments };
