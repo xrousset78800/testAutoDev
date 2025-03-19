eccd0d81a3faf29cfb9386dfa238dea80457bcd8
AutoDev System <autodev@example.com>
2025-03-19 20:00:45 +0000

    lulu >> Ajoute l'appel au fichier JS dans le fichier html. Ajoute un design basic

--- lulu/script.js
+++ lulu/script.js
@@ -1,1 +1,12 @@
 // script.js
+function getWeather() {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Use the data to update your UI or perform other actions
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+getWeather();
